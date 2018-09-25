7829e62ab17c61fd9795fd262b0b83cbff29ca34
wangkaiwd <1162486176@qq.com>
2018-09-25 23:56:09 +0800

    路由Switch匹配demo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import '@/styles/var'
 import '@/styles/mixins'
 import Admin from './admin'
+import RouteExample from '@/pages/demo/route1'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<RouteExample />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/pages/demo/route1.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+      {/* Switch：当对应的路径匹配到对应的组件时，后面的路径不会再进行匹配 */}
+      {/* 下面这种写法，下面三个路由都只会匹配Home组件 */}
+      <Switch>
+        {/* exact:准确的，加了这个属性之后相当与精准匹配 */}
+        {/* 根路径会一直匹配 */}
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Topics} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    {/* <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic} />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h3>Please select a topic.</h3>}
+    /> */}
+  </div>
+);
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
+export default BasicExample;
