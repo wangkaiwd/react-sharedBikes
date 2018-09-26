2d30b14fc06b079f729d35403c5407c193d40adf
wangkaiwd <1162486176@qq.com>
2018-09-26 23:55:27 +0800

    删除根路径'/',以'/app‘进行匹配，并添加404页面

--- src/pages/demo/router.js
+++ src/pages/demo/router.js
@@ -5,6 +5,9 @@ import About from './About'
 import Topics from './Topics'
 import Main from './main'
 import ExampleRoute2 from './route2'
+const Page404 = () => {
+  return <div>404NotFound</div>
+}
 class IRouter extends Component {
   constructor() {
     super()
@@ -22,6 +25,9 @@ class IRouter extends Component {
             )} />
             <Route path="/about" component={About} />
             <Route path="/topics" component={Topics} />
+            {/* 必须和Switch进行配合使用，当写入的路由都没有匹配时进行展示404 */}
+            {/* 如果没有Switch的话，会导致每个页面都匹配404 */}
+            <Route component={Page404} />
           </Switch>
         </ExampleRoute2>
       </Router>
