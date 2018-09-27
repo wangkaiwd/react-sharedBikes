ca558d701a6a865c4f8ec596af92905b57b773be
wangkaiwd <1162486176@qq.com>
2018-09-27 13:38:05 +0800

    使用webstorm对代码进行简单优化

--- src/admin.js
+++ src/admin.js
@@ -1,57 +1,50 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import { Row, Col } from 'antd';
+import { Row, Col } from 'antd'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import NavLeft from '@/components/NavLeft'
 import Home from '@/pages/home'
-class Admin extends Component {
-  constructor() {
-    super()
-    this.state = {
 
-    }
+class Admin extends Component {
+  constructor () {
+    super()
+    this.state = {}
   }
-  render() {
+
+  render () {
     return (
       <Wrapper>
         <Row className="admin">
           <Col span="4" className="nav-left">
-            <NavLeft></NavLeft>
+            <NavLeft/>
           </Col>
           <Col span="20" className="main">
-            <Header></Header>
+            <Header/>
             <Row className="content-wrapper">
               <div className="content">
-                <Home></Home>
+                <Home/>
               </div>
             </Row>
-            <Footer></Footer>
+            <Footer/>
           </Col>
         </Row>
       </Wrapper>
     )
   }
 }
+
 export default Admin
 
 const Wrapper = styled.div`
-  .admin {
-    display: flex;
-  }
+  .admin { display: flex; }
   .nav-left {
     width: 15%;
     min-width: 180px;
     height: 100vh;
   }
-  .content-wrapper {
-    padding: ${themeConfig["@space-lg"]};
-  }
-  .content {
-    background-color: ${themeConfig["@white"]};
-  }
-  .main {
-    flex: 1;
-    background-color: ${themeConfig["@gray"]};
+  .content-wrapper { padding: ${themeConfig['@space-lg']}; }
+  .content { background-color: ${themeConfig['@white']}; }
+  .main { flex: 1; background-color: ${themeConfig['@gray']};
   }
 `
