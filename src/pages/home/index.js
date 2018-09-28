05e9968f19ceab8f5a9ee5a48f92f429d3452221
wangkaiwd <1162486176@qq.com>
2018-09-29 00:03:40 +0800

    删除换肤功能，会造成ant design原始样式出现问题

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import Theme from '@/components/theme'
-import { Card, Button, Spin, Row, Col, DatePicker } from 'antd'
-const { RangePicker } = DatePicker
 class Home extends Component {
   constructor() {
     super()
@@ -11,33 +8,7 @@ class Home extends Component {
   render() {
     return (
       <Wrapper>
-        <div>
-          页面首页，可能要展示一些比较好看的东西
-          <Card
-            title="Card title"
-            bordered={true}
-            style={{ width: 600, marginTop: 20 }}
-          >
-            <Theme />
-            <Button type="primary" style={{ marginTop: '20px' }}>
-              点击上部颜色按钮切换主题
-            </Button>
-            <Row gutter={16} style={{ marginTop: '20px' }}>
-              <Col span="4">
-                <Spin size="small" />
-              </Col>
-              <Col span="4">
-                <Spin />
-              </Col>
-              <Col span="4">
-                <Spin size="large" />
-              </Col>
-              <Col span={12}>
-                <RangePicker size="default" />
-              </Col>
-            </Row>
-          </Card>
-        </div>
+        页面首页，可能要展示一些比较好看的东西
       </Wrapper>
     )
   }
